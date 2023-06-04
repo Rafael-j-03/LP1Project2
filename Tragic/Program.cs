c876e715b7b3c52c800e56d1d8f78244bc2d01f5
Rafael José <ra.david@hotmail.com>
2023-06-04 21:23:32 +0100

    Add project 'Tragic'

---
+++ Tragic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tragic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
