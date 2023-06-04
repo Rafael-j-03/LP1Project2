2a25ad1cf76764b0b42fddaf70b5e1f44c970fdd
Rafael José <ra.david@hotmail.com>
2023-06-04 21:41:33 +0100

    Add card FlyingWand

---
+++ Tragic/FlyingWand.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tragic
+{
+    public class FlyingWand : Card
+    {
+        public string Name => "Flying Wand";
+        public int C => 1;
+        public int AP => 1;
+        public int DP => 1;
+        public int Quantity => 4;
+    }
+}
