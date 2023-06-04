cc1a45d5ca66764d5c7c3c9c740c3c16ccaa24be
Rafael José <ra.david@hotmail.com>
2023-06-04 21:43:11 +0100

    Add card Severed Monkey

---
+++ Tragic/SeveredMonkey.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tragic
+{
+    public class SeveredMonkey : Card
+    {
+        public string Name => "Severed Monkey";
+        public int C => 1;
+        public int AP => 2;
+        public int DP => 1;
+        public int Quantity => 4;
+    }
+}
